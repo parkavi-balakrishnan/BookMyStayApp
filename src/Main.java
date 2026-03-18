a84c902f93ae0701e5dcfd626c51fc3b58a069ff
parkavi-balakrishnan <parkavibalakrishnan68@gmail.com>
2026-03-18 09:23:56 +0530

    UC2:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,35 @@
 abstract class Room {
-    String roomType;
-    int beds;
-    int size;
-    double price;
 
-    public Room(String roomType, int beds, int size, double price) {
-        this.roomType = roomType;
+    private String type;
+    private int beds;
+    private int size;
+    private double price;
+
+    public Room(String type, int beds, int size, double price) {
+        this.type = type;
         this.beds = beds;
         this.size = size;
         this.price = price;
     }
 
-    public void displayDetails(int availability) {
-        System.out.println("Room Type: " + roomType);
+    public String getType() {
+        return type;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayRoom(int availability) {
+        System.out.println("Room Type: " + type);
         System.out.println("Beds: " + beds);
         System.out.println("Size: " + size + " sq ft");
         System.out.println("Price per night: $" + price);
@@ -22,18 +39,21 @@ abstract class Room {
 }
 
 class SingleRoom extends Room {
+
     public SingleRoom() {
-        super("Single Room", 1, 200, 80);
+        super("Single Room", 1, 180, 80);
     }
 }
 
 class DoubleRoom extends Room {
+
     public DoubleRoom() {
-        super("Double Room", 2, 350, 120);
+        super("Double Room", 2, 300, 120);
     }
 }
 
 class SuiteRoom extends Room {
+
     public SuiteRoom() {
         super("Suite Room", 3, 500, 250);
     }
@@ -43,16 +63,16 @@ public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        SingleRoom single = new SingleRoom();
-        DoubleRoom doubleRoom = new DoubleRoom();
-        SuiteRoom suite = new SuiteRoom();
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
         int singleAvailability = 5;
         int doubleAvailability = 3;
         int suiteAvailability = 2;
 
-        single.displayDetails(singleAvailability);
-        doubleRoom.displayDetails(doubleAvailability);
-        suite.displayDetails(suiteAvailability);
+        single.displayRoom(singleAvailability);
+        doubleRoom.displayRoom(doubleAvailability);
+        suite.displayRoom(suiteAvailability);
     }
 }
